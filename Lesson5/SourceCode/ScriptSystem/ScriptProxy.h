e33dcb707e52d6975d273fc767ad532deaff84ab
Max <morgunovmi@yandex.ru>
2022-10-17 21:50:15 +0300

    Remove unused func

--- Lesson5/SourceCode/ScriptSystem/ScriptProxy.h
+++ Lesson5/SourceCode/ScriptSystem/ScriptProxy.h
@@ -13,10 +13,6 @@ public:
 	virtual void Update(float dt) override;
 
 	sol::state& GetLua() { return lua_script; }
-	template<typename T>
-	void Set(std::string_view var, T&& value) {
-		lua_script[var] = std::forward(value);
-	}
 private:
 	sol::state lua_script;
 	sol::protected_function m_update;
